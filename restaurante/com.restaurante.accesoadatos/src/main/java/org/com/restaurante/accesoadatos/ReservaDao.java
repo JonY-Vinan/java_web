8362ad3a2cf0194ab646a3f5b6b2136f37cbf9a2
JonY-Vinan <jonyv.c.ecuador95@gmail.com>
2024-12-20 12:29:37 +0100

    cambios menores

--- restaurante/com.restaurante.accesoadatos/src/main/java/org/com/restaurante/accesoadatos/ReservaDao.java
+++ restaurante/com.restaurante.accesoadatos/src/main/java/org/com/restaurante/accesoadatos/ReservaDao.java
@@ -2,6 +2,6 @@ package org.com.restaurante.accesoadatos;
 
 import org.com.restaurante.entidades.Reserva;
 
-public interface DaoReserva extends Dao<Reserva>{
+public interface ReservaDao extends Dao<Reserva>{
 
 }
